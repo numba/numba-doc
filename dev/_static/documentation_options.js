4fecb38226f11b1c097a05b5b6bec81659ea4464
Stan Seibert <sseibert@anaconda.com>
2020-04-29 17:21:17 -0500

    Updated doc release: dev

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.50.0.dev0+158.g01310bd-py3.7-linux-x86_64.egg',
+    VERSION: '0.50.0.dev0+164.gb2e25bd-py3.7-linux-x86_64.egg',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
